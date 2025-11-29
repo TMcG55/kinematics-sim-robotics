e92843715affb319f6e68b98790632a1147e43d2
Tom McGuinness <tmcguinnesst@gmail.com>
2025-11-29 18:12:25 +0000

    basic 3d rotations using ZYX Euler angles implemented

--- KinematicsSim.c
+++ KinematicsSim.c
@@ -1,68 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>>
 
 #ifndef M_PI
 #define M_PI (3.14159265358979323846264338327950288)
 #endif
 
-#define R1 3 // number of rows in Matrix-1
+#define R1 3  //number of rows in Matrix-1
 #define R2 3 
 #define C1 3 
 #define C2 1 
 
-void multiplyMatrix(double rotation_matrix[][C1], double vector[][C2])
+void multiplyMatrix(double rotation_matrix[][3], double p_zero[3],double p_transformed[3])
 {
-    int result[R1][C2];
-    printf("resultant matrix is:\n");
-    for(int i=0;i<R1;i++){
-        for(int j=0;j<C2;j++){
-            result[i][j]=0;
-            for(int k=0;k<R2;k++){
-                result[i][j] += rotation_matrix[i][k]*vector[k][j];
-            }
-            printf("%d\t",result[i][j]);
+    printf("p_zero\n");
+    for (int i=0;i<3;i++)
+        printf(" %f \n",p_zero[i]);
+
+    printf("rotation_matrix \n");
+    for (int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+            printf(" %f ",rotation_matrix[i][j]);
         }
         printf("\n");
     }
-}
+    printf("p_transformed\n");    
+    for (int i=0;i<3;i++){
+        p_transformed[i]=0;
+        for (int j=0;j<3;j++){
+            p_transformed[i]+=p_zero[j]*rotation_matrix[i][j];
+        }
+        printf("%f\n",p_transformed[i]);
+    }
 
-void eulerZ(double vector[][C2], double alpha)
+} 
+
+
+void eulerZ(double p_zero[3], double alpha, double p_transformed[3])
 {
+
     double c_alpha = cos(alpha);
     double s_alpha = sin(alpha);
     double rotation_z[R1][C1] = {{c_alpha,-s_alpha,0},{s_alpha,c_alpha,0},{0,0,1}};
 
-    multiplyMatrix(rotation_z, vector);
+    multiplyMatrix(rotation_z, p_zero,p_transformed);
+}
+void eulerY(double p_zero[3], double beta, double p_transformed[3])
+{
+    double c_alpha = cos(beta);
+    double s_alpha = sin(beta);
+    double rotation_y[R1][C1] = {{c_alpha,-s_alpha,0},{s_alpha,c_alpha,0},{0,0,1}};
 
+    multiplyMatrix(rotation_y, p_zero,p_transformed);
+}
+void eulerX(double p_zero[3], double gamma, double p_transformed[3])
+{
+    double c_alpha = cos(gamma);
+    double s_alpha = sin(gamma);
+    double rotation_z[R1][C1] = {{c_alpha,-s_alpha,0},{s_alpha,c_alpha,0},{0,0,1}};
+
+    multiplyMatrix(rotation_z, p_zero,p_transformed);
 }
 
 int main()
 {
-    double  angle=M_PI;
-    double  s_alpha=sin(angle);
-    double  c_alpha=cos(angle);
-    double* alpha = &angle;
 
-    double rotation_matrix[R1][C1] = {{c_alpha,-s_alpha,0},{s_alpha,c_alpha,0},{0,0,1}};
-    double vector[R2][C2] = {{2},{2},{1}};
+    double  alpha=M_PI/2;
+    double  beta=M_PI/2;
+    double  gamma=M_PI/2;
+    double  s_alpha=sin(alpha);
+    double  c_alpha=cos(alpha);
 
-    // if coloumn of rotation_matrix not equal to rows of vector
-    if (C1 != R2) {
-        printf("The number of columns in Matrix-1 must be "
-               "equal to the number of rows in "
-               "Matrix-2\n");
-        printf("Please update MACROs value according to "
-               "your array dimension in "
-               "#define section\n");
+    double p_zero[3] = {2,2,1};
 
-        exit(EXIT_FAILURE);
-    }
+    double p_transformed[3]={0,0,0};
 
-    // Function call
-    eulerZ(vector,*alpha);
-    eulerY(vector,*alpha);
-    eulerX(vector,*alpha);
+    //  Function call
+    printf("----------------------------------\n");
+    printf("Z rotate\n");
+    printf("----------------------------------\n");
+    eulerZ(p_zero,alpha,p_transformed);
+
+    printf("----------------------------------\n");
+    printf("Y rotate\n");
+    printf("----------------------------------\n");
+    memcpy(p_zero, p_transformed, sizeof(*p_transformed));
+    eulerY(p_zero,beta,p_transformed);
+
+
+    for (int i=0;i<3;i++)
+        p_zero[i]=p_transformed[i];
+    printf("----------------------------------\n");
+    printf("Z rotate\n");
+    printf("----------------------------------\n");
+    eulerZ(p_zero,gamma,p_transformed);
+    
 
     return 0; 
 }
